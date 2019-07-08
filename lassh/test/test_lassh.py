b76490597595f6d34c9ad56c80f39c93f5ef7237
Steven Chun <steven.r.chun.19@dartmouth.edu>
2019-07-08 11:36:18 -0500

    add addhost test

--- lassh/test/test_lassh.py
+++ lassh/test/test_lassh.py
@@ -10,8 +10,11 @@ class TestLasshFile(unittest.TestCase):
 
     def setUp(self):
         self.runner = CliRunner()
-        # mock pathlib's expanduser to use the temp directory
 
+    def __init_lassh_config(self):
+        print('sec')
+
+    # mock pathlib's expanduser to use the temp directory
     @unittest.mock.patch('lassh.HOME_SSH_DIR_PATH', Path("./.ssh"))
     @unittest.mock.patch('lassh.HOME_SSH_CONFIG_PATH', Path("./.ssh/config"))
     def test_init(self):
@@ -23,3 +26,26 @@ class TestLasshFile(unittest.TestCase):
             expected_contents = 'Include {0}/lassh.config\n'.format(
                 os.getcwd())
             self.assertEqual(file_contents, expected_contents)
+
+    """
+    read lassh.config file, iterate backwards until you get to the first line
+    starting with Host(space)
+    then compare with expected string
+    """
+
+    def test_addhost(self):
+        # create lassh.config
+        with self.runner.isolated_filesystem():
+            self.runner.invoke(lassh, ['init'])
+
+            self.runner.invoke(
+                lassh, ['addhost', 'snafu', 'foobar@dartmouth.edu', 'sally'])
+
+            with open("./lassh.config", 'r+') as config:
+                config_contents = config.read()
+
+            expected_contents = (
+                '\nHost snafu\n  HostName foobar@dartmouth.edu\n  User sally\n'
+            )
+
+        self.assertEqual(config_contents, expected_contents)
